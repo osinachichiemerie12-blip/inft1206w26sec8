50b48da5469c51b7ba07ce3ac4e7dc3ba051f97a
osinachichiemerie12-blip <osinachichiemerie12@gmail.com>
2026-04-10 01:37:21 -0400

    this is my starting file

---
+++ Assignment4/part3/JS/P3.js
@@ -0,0 +1,26 @@
+/*
+Name: Osinach-Nnedu Chiemerie
+File: p3.js
+Date: 9 April 2026
+Assignment 4 part 3
+*/
+// setup canvas
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
