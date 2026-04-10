c0d840ef196161d020ea77d15028d883a3700454
osinachichiemerie12-blip <osinachichiemerie12@gmail.com>
2026-04-10 02:15:30 -0400

    finished the collision detect function

--- Assignment4/part3/JS/P3.js
+++ Assignment4/part3/JS/P3.js
@@ -64,6 +64,19 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
 }
 
 // Animating the balls
@@ -91,10 +104,14 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
 }
 
 loop();
+
+// Adding collision detection
+
 
