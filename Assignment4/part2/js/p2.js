59e2b07b3062d731a7f51410fddbbf851aca6799
osinachichiemerie12-blip <osinachichiemerie12@gmail.com>
2026-04-10 00:21:09 -0400

    added images to the thumbnail bar

--- Assignment4/part2/js/p2.js
+++ Assignment4/part2/js/p2.js
@@ -12,11 +12,28 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 // Creating a data object
-const images = [
+var images = [
     {filename: "pic1.jpg", alt: "Closeup of a human eye"},
     {filename: "pic2.jpg", alt: "Rock that looks like a wave"},
     {filename: "pic3.jpg", alt: "Purple and white pansies"},
     {filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb"},
     {filename: "pic5.jpg", alt: "Large moth on a leaf"}
 ]
+
+// Adding images to the thumbnail
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/" 
+for (let image of images) {
+    // Creating new image element
+    let newImg = document.createElement("img");
+    newImg.src = `${baseURL}${image.filename}`;
+    newImg.alt = image.alt;
+    newImg.tabIndex = "0";
+    thumbBar.appendChild(newImg);
+    newImg.addEventListener("click", updateDisplayedImage);
+    newImg.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
+}
 
