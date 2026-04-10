edfd158437b298937c5130d15367b778a1fc48e0
osinachichiemerie12-blip <osinachichiemerie12@gmail.com>
2026-04-10 03:13:29 -0400

    updated the loop function

--- Assignment4/part4/JS/p4.js
+++ Assignment4/part4/JS/p4.js
@@ -165,15 +165,22 @@ while (balls.length < 25) {
 
     balls.push(ball);
 }
+const evilBall = new EvilCircle(random(0, width), random(0, height));
 function loop() {
     ctx.fillStyle = "rgb(0 0 0 / 25%)";
     ctx.fillRect(0, 0, width, height);
 
     for (const ball of balls) {
-        ball.draw();
-        ball.update();
-        ball.collisionDetect();
+        if (ball.exists) {
+            ball.draw();
+            ball.update();
+            ball.collisionDetect();
+        }
     }
+
+    evilBall.draw();
+    evilBall.checkBounds();
+    evilBall.collisionDetect();
 
     requestAnimationFrame(loop);
 }
