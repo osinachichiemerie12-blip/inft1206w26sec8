4edb00581fb360a07656f868ecbcb8e556ed11c6
osinachichiemerie12-blip <osinachichiemerie12@gmail.com>
2026-04-10 03:08:44 -0400

    finished with the collision detect function

--- Assignment4/part4/JS/p4.js
+++ Assignment4/part4/JS/p4.js
@@ -132,6 +132,19 @@ class EvilCircle extends Shape {
             this.y += this.size;
         }
     }
+    collisionDetect() {
+        for (const ball of balls) {
+            if (ball.exists) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + ball.size) {
+                    ball.exists = false;
+                }
+            }
+        }
+    }
 }
 
 // Animating the balls
